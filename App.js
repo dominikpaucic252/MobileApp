bde58e8d7893df908259e2955e045321e96a6949
Dominik <D.Paucic.Inf21@stud.bbbaden.ch>
2023-10-11 00:35:11 +0200

    Pedometer built in

--- App.js
+++ App.js
@@ -1,11 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { Pedometer } from 'expo-sensors';
+console.log(Pedometer);
+
 
 export default function App() {
+  const [steps, setSteps] = useState(0);
+  const [isTracking, setIsTracking] = useState(false);
+  let _subscription = null;
+
+  useEffect(() => {
+    return () => {
+      if (_subscription) {
+        _subscription.remove();
+        _subscription = null;
+      }
+    };
+  }, []);
+
+  const _subscribe = () => {
+    _subscription = Pedometer.watchStepCount(result => {
+        setSteps(result.steps);
+    });
+    
+    setIsTracking(true);
+};
+
+  const _unsubscribe = () => {
+    if (_subscription) {
+      _subscription.remove();
+      _subscription = null;
+      setIsTracking(false);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>StepAndTrack App</Text>
+      <Text style={styles.label}>Schritte:</Text>
+      <Text style={styles.value}>{steps}</Text>
+
+      <Button 
+        title={isTracking ? "Stop Tracking" : "Starte Tracking"} 
+        onPress={() => {
+          if (isTracking) {
+            _unsubscribe();
+          } else {
+            _subscribe();
+          }
+        }} 
+      />
     </View>
   );
 }
@@ -16,5 +60,21 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  label: {
+    fontSize: 18,
+    marginBottom: 5,
+  },
+  value: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 15,
   },
 });
+
